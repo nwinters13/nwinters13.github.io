eaf27d12b821d47f8bb510a1d73891a8bdeed195
nwinters13 <nathan.winters@tufts.edu>
2014-12-23 22:26:23 -0500

    big push...starting about me

--- homepage.js
+++ homepage.js
@@ -3,7 +3,7 @@ jQuery("document").ready(function($){
 	var nav = $('.nav-container');
 	
 	$(window).scroll(function () {
-		if ($(this).scrollTop() > 1) {
+		if ($(this).scrollTop() > 20) {
 			nav.addClass("f-nav");
 		} else {
 			nav.removeClass("f-nav");
---
+++ onload.js
@@ -0,0 +1,16 @@
+var width = $('.funPic').width();
+$('.funPic').css('height', width);
+var width = $('.funPicCenter').width();
+$('.funPicCenter').css('height', width);
+var navHeight = #('.navContainer').height();
+$('.navBarDrop').css('height', navHeight);
+
+
+$(window).resize(function() {
+	var width = $('.funPic').width();
+	$('.funPic').css('height', width);
+	width = $('.funPicCenter').width();
+	$('.funPicCenter').css('height', width);
+	var navHeight = #('.navContainer').height();
+	$('.navBarDrop').css('height', navHeight);
+});
